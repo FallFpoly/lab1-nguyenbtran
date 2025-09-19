9d3fdbf98c02989a8238d072a39ded69f09ffba4
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-09-19 22:03:33 +0700

    Lab1_bai2

--- Lab1/src/main/java/com/mycompany/lab1/bai2.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai2.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.lab1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -12,5 +14,18 @@ public class bai2 {
 
     public static void main(String[] args) {
           // sinh viên code ở đây
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chiều dài: ");
+        double dai = scanner.nextDouble();
+
+        System.out.print("Nhập chiều rộng: ");
+        double rong = scanner.nextDouble();
+        double chuVi = (dai + rong) * 2;
+        double dienTich = dai * rong;
+        double canhNho = Math.min(dai, rong);
+
+        System.out.println("Chu vi hình chữ nhật = " + chuVi);
+        System.out.println("Diện tích hình chữ nhật = " + dienTich);
+        System.out.println("Cạnh nhỏ nhất = " + canhNho);
     }
 }
