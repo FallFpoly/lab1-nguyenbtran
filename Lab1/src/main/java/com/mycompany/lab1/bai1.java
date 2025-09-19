838157c29c9cb515e9388bf3189dbd7dc75456b9
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-09-19 22:03:07 +0700

    Lab1_bai1

--- Lab1/src/main/java/com/mycompany/lab1/bai1.java
+++ Lab1/src/main/java/com/mycompany/lab1/bai1.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.lab1;
-
+import java.util.Scanner;
 /**
  *
  * @author ICT
@@ -12,5 +12,11 @@ public class bai1 {
 
     public static void main(String[] args) {
         // sinh viên code ở đây
+         Scanner scanner = new Scanner(System.in);
+        System.out.print("Họ và tên: ");
+        String hoTen = scanner.nextLine();
+        System.out.print("Điểm TB: ");
+        double diemTB = scanner.nextDouble();
+        System.out.printf("%s %.2f điểm", hoTen, diemTB);
     }
 }
